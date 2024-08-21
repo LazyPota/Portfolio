5ba01c6760d21bf50d2b74b6eb0a7ae368e39cee
LazyPota <raflipersada618@gmail.com>
2024-08-21 20:38:25 +0700

    "Push number 2 bro"

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import express = require("express");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
